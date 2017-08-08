d253ffbb7dcdaf83bf720f70fb41ab7d75a40b6d
Selva <k.n.selvakumar@gmail.com>
2017-08-08 19:17:02 +0530

    Using command line parser

--- main.go
+++ main.go
@@ -2,40 +2,49 @@ package main
 
 import (
 	"net"
-	"p2p-port-forward/forwarder"
 	"p2p-port-forward/utils"
 	"github.com/selvakn/libzt"
 	"fmt"
-	"p2p-port-forward/listener"
 	"github.com/google/logger"
 	"os"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-const PORT = 7878
-const NETWORK_ID = "8056c2e21c000001"
+const INTERNAL_ZT_PORT = 7878
 
 func dialLocalService() (net.Conn, error) {
-	return net.Dial("tcp", fmt.Sprintf("localhost:%s", listener.LocalPortToListen()))
+	return net.Dial("tcp", fmt.Sprintf("localhost:%s", *forwardPort))
 }
 
 func dialRemoteThroughTunnel(zt *libzt.ZT) func() (net.Conn, error) {
 	return func() (net.Conn, error) {
-		conn, err := zt.Connect6(forwarder.GetOtherIP(), PORT)
+		conn, err := zt.Connect6(*serverIp, INTERNAL_ZT_PORT)
 		conn = (&utils.DataRateLoggingConnection{}).Init(conn)
 		return conn, err
 	}
 }
 
+var (
+	network     = kingpin.Flag("network", "zerotier network id").Short('n').Default("8056c2e21c000001").String()
+	mode        = kingpin.Flag("listen-mode", "listen mode").Short('l').Default("false").Bool()
+	forwardPort = kingpin.Flag("forward-port", "port to forward (in listen mode)").Short('f').Default("22").String()
+	acceptPort  = kingpin.Flag("accept-port", "port to accept (in connect mode)").Short('a').Default("2222").String()
+
+	serverIp = kingpin.Flag("server", "server (zerotier) ip").Short('s').String()
+)
+
 func main() {
 	logger.Init("p2p-port-forward", false, false, os.Stdout)
 
-	zt := libzt.Init(NETWORK_ID, "./zt")
+	kingpin.Parse()
+
+	zt := libzt.Init(*network, "./zt")
 
 	logger.Infof("ipv4 = %v \n", zt.GetIPv4Address().String())
 	logger.Infof("ipv6 = %v \n", zt.GetIPv6Address().String())
 
-	if len(forwarder.GetOtherIP()) == 0 {
-		listener, _ := zt.Listen6(PORT)
+	if *mode {
+		listener, _ := zt.Listen6(INTERNAL_ZT_PORT)
 		loggingListener := &utils.LoggingListener{Listener: listener}
 		dataRageLogginglistener := &utils.DataRateLoggingListener{Listener: loggingListener}
 
@@ -46,7 +55,7 @@ func main() {
 		})
 
 	} else {
-		ln, _ := net.Listen("tcp", fmt.Sprintf(":%s", forwarder.LocalPortToForward()))
+		ln, _ := net.Listen("tcp", fmt.Sprintf(":%s", *acceptPort))
 
 		go utils.Sync(ln.Accept, dialRemoteThroughTunnel(zt))
 
